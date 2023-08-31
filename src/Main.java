4d0bf932c4ab8abf3f02f66ec1318688d50a4a4b
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-08-31 20:42:01 +0300

    add error message with wrong date format

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,7 +17,7 @@ public class Main extends JFrame {
     JTextArea textArea1;
     int buttonClicked;
 
-    List<String> dates;
+    List<MyDateClass> dates = new ArrayList<MyDateClass>();
 
     public static void main(String[] args) {
         // define an instance of the Main class (which is the window class)
@@ -24,7 +25,7 @@ public class Main extends JFrame {
     }
 
     public Main() {
-        this.setSize(400,400);
+        this.setSize(400, 400);
         this.setResizable(false);
         this.setTitle("my nice GUI");
         // put window in center
@@ -55,7 +56,7 @@ public class Main extends JFrame {
         panel.add(textField1);
 
         // text area
-        textArea1 = new JTextArea(15,22);
+        textArea1 = new JTextArea(15, 22);
         textArea1.setLineWrap(true);
         // Makes sure that words stay intact if a line wrap occurs
         textArea1.setWrapStyleWord(true);
@@ -65,6 +66,7 @@ public class Main extends JFrame {
         ListenForButton lForButton = new ListenForButton();
 
         addButton.addActionListener(lForButton);
+        sortButton.addActionListener(lForButton);
         // add the panel
         this.add(panel);
 
@@ -74,18 +76,44 @@ public class Main extends JFrame {
     private class ListenForButton implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == addButton) {
-                if (MyDateClass.checkFormat(textField1.getText())){
+                if (MyDateClass.checkFormat(textField1.getText())) {
+                    // create an object of MyDateClass
                     MyDateClass date = new MyDateClass(textField1.getText());
-                    textArea1.append(String.valueOf(date.day));
-                    textArea1.append(String.valueOf(date.month));
-                    textArea1.append(String.valueOf(date.year));
-                }
-                else {
-                    // show an error message
+
+                    // add the new date to the list
+                    dates.add(date);
+                } else {
+                    // show an error box
+                    JOptionPane.showMessageDialog(new JFrame(), "Wrong date format please use dd/MM/yyyy", "Error",
+                            JOptionPane.ERROR_MESSAGE);
                 }
             } else if (e.getSource() == sortButton) {
-                textArea1.append("Sort");
+
+                // sort using O(n^2)
+                for (int i = 0; i < dates.size(); i++) {
+                    for (int j = i + 1; j < dates.size(); j++) {
+                        if (dates.get(i).year > dates.get(j).year) {
+                            MyDateClass tempDate = dates.get(i);
+                            dates.set(i, dates.get(j));
+                            dates.set(j, tempDate);
+                        }
+                    }
+                }
             }
+
+            //clear the text area always
+            textArea1.setText("");
+            // then fill it with the current list of dates we have (always if we add or sort or do nothing)
+            for (
+                    int i = 0; i < dates.size(); i++) {
+                textArea1.append(dates.get(i).toString());
+
+                // if it's not last element, add a new line
+                if (i + 1 != dates.size()) {
+                    textArea1.append("\n");
+                }
+            }
+
         }
     }
 }
@@ -102,6 +130,12 @@ class MyDateClass {
         month = Integer.parseInt(parts[1]);
         year = Integer.parseInt(parts[2]);
     }
+
+    public String toString() {
+
+        return String.valueOf(day) + "/" + String.valueOf(month) + "/" + String.valueOf(year);
+    }
+
     public static boolean checkFormat(String s) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         dateFormat.setLenient(false);
